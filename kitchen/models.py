67fed9de35ff3e10776bea47a97b0bf7fff0f1b8
Vik <viktoria.om@gmail.com>
2025-07-15 23:39:51 +0300

    create DishType, Dish, Ingredient, Cook models & change AUTH_USER_MODEL to Cook

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,43 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name", ]
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    dish_type = models.ForeignKey(DishType, related_name="dishes", on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name", ]
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    dishes = models.ManyToManyField(Dish, related_name="ingredients")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name", ]
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        ordering = ["username", ]
