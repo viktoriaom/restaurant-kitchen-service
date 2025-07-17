130d9fde3e67649ad695c340bdb3364ff722ab30
Vik <viktoria.om@gmail.com>
2025-07-17 19:01:52 +0300

    create index function in kitchen.views

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from kitchen.models import Cook, Dish, Ingredient, DishType
+
+
 # Create your views here.
+def index(request):
+    num_cooks = Cook.objects.all().count()
+    num_dishes = Dish.objects.all().count()
+    num_categories = DishType.objects.all().count()
+    num_ingredients = Ingredient.objects.all().count()
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_categories": num_categories,
+        "num_ingredients": num_ingredients,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
