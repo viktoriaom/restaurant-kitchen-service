3340cb0bc7fa85fdcd2f9c1ac1353486b0ce8bdd
Vik <viktoria.om@gmail.com>
2025-08-06 13:02:57 +0300

    import kitchen.signals to apps.py to ensure signals get loaded

--- kitchen/apps.py
+++ kitchen/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class KitchenConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'kitchen'
+
+    def ready(self):
+        import kitchen.signals  # noqa Ensure signals get loaded
